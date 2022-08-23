fe71e868f0e5d4a82e32ff796fcb9da7afd3ec02
sajitha-eglobeits <sajitha@eglobeits.com>
2022-08-23 17:09:27 +0530

    pdp updation - select box for qty,price related text

---
+++ app/code/Moduletest/VendorTest/registration.php
@@ -0,0 +1,11 @@
+ <?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+
+    'Moduletest_VendorTest',
+
+    __DIR__
+
+);
